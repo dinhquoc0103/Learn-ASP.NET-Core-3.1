5fd74f3986a5c5a78f033ca959d6b2e65a48ed43
TienQuach <tienquach@sorimachigroup.vn>
2021-04-09 17:01:39 +0700

    mot phan handler

---
+++ S02_RazorPages/Model_Class/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Model_Class.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{this.FirstName} {this.LastName}";
+        [ViewData]
+        public string Message { get; set; }
+
+        public string SayGoodbye() => $"Hello, {this.FullName}! I'm in the Model Class";
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            this.FirstName = "Donald";
+            this.LastName = "Donald";
+
+            // Index.cshtml.cs
+
+                ViewData["MyNumber"] = 2020;
+                ViewData["MyString"] = "Hello World, Razor Pages!";
+            //ViewData["MyObject"] = new Book
+            //{
+            //    Title = "ASP.NET Core for Dummy",
+            //    Publisher = "Tự học ICT Press",
+            //    Author = "Mai Chi"
+            //};
+
+            this.Message = "Hello lo";
+       
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Pages/Multiform.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Pages
+{
+    public class MultiformModel : PageModel
+    {
+        public string Message { get; set; } = "Initial Request";
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            this.Message = "Form Posted";
+        }
+        public void OnPostDelete()
+        {
+            this.Message = "Delete Handler Fired";
+        }
+        public void OnPostEdit()
+        {
+            this.Message = "Edit Handler Fired";
+        }
+        public void OnPostView()
+        {
+            this.Message = "View Handler Fired";
+        }
+    }
+}
