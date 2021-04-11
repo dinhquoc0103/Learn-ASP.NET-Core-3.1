e37e74f915ced05ee3793c7b1ccb98817983b9cc
quoc le <dinhquoc0103@gmail.com>
2021-04-11 15:24:13 +0700

    co ban hieu form post

---
+++ POST/WebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Title { get; private set; }
+        public string Text { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        // Sử dụng tham số của hàm truy vấn OnPost để nhận dữ liệu từ thuộc tính name khi form submit
+        //public void OnPost(string title, string text)
+        //{
+        //    this.Title = title;
+        //    this.Text = text;
+        //}
+
+        // Hoặc cũng có thể nhận giá trị từ form thông qua thuộc tính name bằng cách sử dụng thuộc tính Form của đối tượng Request
+        public void OnPost()
+        {
+            this.Title = Request.Form["title"];
+            this.Text = Request.Form["text"];
+        }
+
+
+    }
+}
